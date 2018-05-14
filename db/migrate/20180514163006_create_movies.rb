82c0fb28f86fd03611c7fea8911897e34fffa401
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-14 16:31:29 +0000

    automatically backed up by learn

--- db/migrate/20180514163006_create_movies.rb
+++ db/migrate/20180514163006_create_movies.rb
@@ -1,4 +1,11 @@
 class CreateMovies < ActiveRecord::Migration
   def change
+    create_table :moves do |t|
+      t.string :title
+      t.datetime :release_date
+      t.string :director
+      t.string :lead_actor_or_actress
+      t.boolean :in_theaters 
+    end
   end
 end
