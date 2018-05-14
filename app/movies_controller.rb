bab7803a3065b2f03ba6f21a2daed8643d5e58b3
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-14 17:13:16 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -26,8 +26,9 @@ end
 def can_be_created_in_a_block(args = nil)
 
   Movie.create do |m|
-   m.title = title 
-   m.release_date = year 
+   m.title = title
+   m.release_date = year
+   
   end
 end
 
